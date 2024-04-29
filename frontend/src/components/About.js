8e3e9e3a4a9404f271cd709fd0936e995c1ad225
Nathan Krieger <124911662+nathankrieger@users.noreply.github.com>
2024-04-29 12:01:02 -0500

    Functional search bar

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -7,7 +7,7 @@ class About extends Component {
         return (
             <div>
                 <h1><strong>SE/ComS319 Construction of User Interfaces, Spring 2024</strong></h1>
-                <p class="lead">03/09/2024 <br></br> Professor Jannesari</p>
+                <p class="lead">05/08/2024 <br></br> Professor Jannesari</p>
                 <h2>Nathan Krieger</h2>
                 <p class="lead">Sophomore studying Software Engineering at Iowa State University.<br></br>
                     <a href="mailto:nkrieger@iastate.edu"><i class="fas fa-envelope"></i> nkrieger@iastate.edu</a> <br></br>
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,11 +10,11 @@ class NavBar extends Component {
         return (
             <header className="mb-auto">
                 <div>
-                    <h3 className="float-md-start mb-0">Courses</h3>
+                    <h3 className="float-md-start mb-0">Course Tracker</h3>
                     <nav className="nav nav-masthead justify-content-center float-md-end">
-                        <Link to="/" className="nav-link fw-bold py-1 px-0" aria-current="page">Home</Link>
-                        <Link to="/getCatalog" className="nav-link fw-bold py-1 px-0">Courses</Link>
-                        <Link to="/flowcharts" className="nav-link fw-bold py-1 px-0">Flowcharts</Link>
+                        <Link to="/" className="nav-link fw-bold py-1 px-0">Home</Link>
+                        {/* <Link to="/getCatalog" className="nav-link fw-bold py-1 px-0">Courses</Link> */}
+                        {/* <Link to="/flowcharts" className="nav-link fw-bold py-1 px-0">Flowcharts</Link> */}
                         <Link to="/about" className="nav-link fw-bold py-1 px-0">About</Link>
                     </nav>
                 </div>
